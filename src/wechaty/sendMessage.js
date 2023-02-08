afa90b84343f7beeba802af19ae92c356ffebfcb
fuyunde <fuyunde@foxmail.com>
2023-02-08 09:25:29 +0800

    improvement: 限制只回复服务启动后并且三分钟内接收到的消息

--- src/wechaty/sendMessage.js
+++ src/wechaty/sendMessage.js
@@ -2,6 +2,8 @@
 import { getOpenAiReply as getReply } from '../openai/index.js'
 import { botName, roomWhiteList, aliasWhiteList } from '../../config.js'
 
+const serviceStartTime = new Date();
+
 /**
  * 默认消息发送
  * @param msg
@@ -10,23 +12,26 @@ import { botName, roomWhiteList, aliasWhiteList } from '../../config.js'
  */
 export async function defaultMessage(msg, bot) {
   const contact = msg.talker() // 发消息人
+  const sendTime = msg.date() // 发消息时间
+  const msgAge = msg.age()  // 消息接收的时长，即当有人发送消息到机器人接收到消息之前的间隔，单位（seconds）
   const receiver = msg.to() // 消息接收人
   const content = msg.text() // 消息内容
+  const isMentionSelf = await msg.mentionSelf() // 是否@自己
   const room = msg.room() // 是否是群消息
   const roomName = (await room?.topic()) || null // 群名称
   const alias = (await contact.alias()) || (await contact.name()) // 发消息人昵称
   const remarkName = await contact.alias() // 备注名称
   const name = await contact.name() // 微信名称
   const isText = msg.type() === bot.Message.Type.Text // 消息类型是否为文本
-  const isRoom = roomWhiteList.includes(roomName) && content.includes(`${botName}`) // 是否在群聊白名单内并且艾特了机器人
+  const isRoom = roomWhiteList.includes(roomName) && isMentionSelf // 是否在群聊白名单内并且艾特了机器人
   const isAlias = aliasWhiteList.includes(remarkName) || aliasWhiteList.includes(name) // 发消息的人是否在联系人白名单内
   const isBotSelf = botName === remarkName || botName === name // 是否是机器人自己
   // TODO 你们可以根据自己的需求修改这里的逻辑
-  if (isText && !isBotSelf) {
+  if (isText && !isBotSelf && sendTime > serviceStartTime && msgAge < 180) {
     try {
       // 区分群聊和私聊
       if (isRoom && room) {
-        await room.say(await getReply(content.replace(`${botName}`, '')))
+        await room.say(await getReply(content.replace(`@${botName}`, '')))
         return
       }
       // 私人聊天，白名单内的直接发送
